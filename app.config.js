08d61a217a1bc21bad6943d24e02316534528224
chinmayjoshi03 <chinmayjoshi003@gmail.com>
2025-10-03 20:01:48 +0530

    app version

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: "strmly",
     scheme: "strmly",
     owner: "strmly-technologies",
-    version: "1.0.5",
+    version: "1.0.6",
     orientation: "portrait",
     icon: "./assets/images/logo3.jpg",
     userInterfaceStyle: "dark",
