a7c5dee6e35651dc68cf09250b554cef60cd895c
chinmayjoshi03 <chinmayjoshi003@gmail.com>
2025-10-07 22:15:54 +0530

    build number 6

--- app.config.js
+++ app.config.js
@@ -13,7 +13,7 @@ export default {
     newArchEnabled: true,
     ios: {
       bundleIdentifier: "com.anonymous.strmly",
-      buildNumber: "5",
+      buildNumber: "6",
       supportsTablet: true,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
